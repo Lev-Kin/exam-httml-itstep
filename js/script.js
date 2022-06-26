103ee5dcd0970c504d091c063db5d5d99687441c
Lev-Kin <57681593+Lev-Kin@users.noreply.github.com>
2022-06-26 12:31:14 +0300

    files add for site

---
+++ js/script.js
@@ -0,0 +1,36 @@
+
+function ibg() {
+    let ibg = document.querySelectorAll(".ibg");
+    for (var i = 0; i < ibg.length; i++) {
+        if (ibg[i].querySelector('img')) {
+            ibg[i].style.backgroundImage = 'url(' + ibg[i].querySelector('img').getAttribute('src') + ')';
+        }
+    }
+}
+
+ibg();
+
+
+$(document).ready(function () {
+    $('.icon-menu').click(function (event) {
+        $(this).toggleClass('active');
+        $('.menu__body').toggleClass('active');
+        $('.header__logo').toggleClass('active');
+        $('.menu__link').addClass('burgered');
+        $('body').toggleClass('lock');
+
+    });
+    $('.menu__link').click(function (event) {
+        $('.icon-menu').removeClass('active');
+        $('.menu__body').removeClass('active');
+        $('.header__logo').removeClass('active');
+        $('body').removeClass('lock');
+    })
+});
+
+if ($('.menu__body').hasClass('active')) {
+    $('body').addClass('lock');
+}
+else {
+    $('body').removeClass('lock');
+}
